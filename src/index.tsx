8f951955ff462649ae536f7cdf524af446964d76
Alexander Lukomsky <lukomsky.alexander@gmail.com>
2023-07-31 13:30:46 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import reportWebVitals from "./reportWebVitals";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -17,5 +16,3 @@ root.render(
     </HashRouter>
   </Provider>
 );
-
-reportWebVitals();
